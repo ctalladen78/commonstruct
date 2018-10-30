8d7ca383c446270f9b17331c84fddbeaa98023d0
Kendrick Kesley <3642811@student.rmit.edu.au>
2018-10-30 16:03:33 +1100

    include job

--- itcjob/Job.go
+++ itcjob/Job.go
@@ -1,9 +1,10 @@
 package itcjob
 
-//JobStatus holds the job status struct.
-type JobStatus struct {
-	JobID     string `json:"job_id"`    //id of the job
-	Principal string `json:"principal"` //arn of the requester
-	Status    string `json:"status"`    //status of the job
-	JobClass  string `json:"job_class"` //where the job comes from (e.g. importing user from excel, making report, etc)
+//Job holds the job
+type Job struct {
+	JobID     string `json:"job_id"`     //id of the job
+	Principal string `json:"principal"`  //arn of the requester
+	Service   string `json:"service"`    //where the job comes from (e.g. importing user from excel, making report, etc)
+	Bucket    string `json:"bucket"`     //bucket of event
+	BucketKey string `json:"bucket_key"` //bucket key
 }
---
+++ itcjob/JobStatus.go
@@ -0,0 +1,9 @@
+package itcjob
+
+//JobStatus holds the job status struct.
+type JobStatus struct {
+	JobID     string `json:"job_id"`    //id of the job
+	Principal string `json:"principal"` //arn of the requester
+	Status    string `json:"status"`    //status of the job
+	Service   string `json:"service"`   //where the job comes from (e.g. importing user from excel, making report, etc)
+}
