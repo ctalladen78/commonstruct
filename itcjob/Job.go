979edf7f9dc51338ad0b0d4faa6e33aa3f0482ca
Kendrick Kesley <3642811@student.rmit.edu.au>
2018-10-31 16:25:05 +1100

    body

--- itcjob/Job.go
+++ itcjob/Job.go
@@ -2,10 +2,12 @@ package itcjob
 
 //Job holds the job
 type Job struct {
-	JobID       string `json:"job_id"`       //id of the job
-	Principal   string `json:"principal"`    //arn of the requester
-	Service     string `json:"service"`      //where the job comes from (e.g. importing user from excel, making report, etc)
-	Bucket      string `json:"bucket"`       //bucket of event
-	BucketKey   string `json:"bucket_key"`   //bucket key
-	AccessToken string `json:"access_token"` //Access Token of the user
+	JobID       string                 `json:"job_id"`       //id of the job
+	Principal   string                 `json:"principal"`    //arn of the requester
+	Service     string                 `json:"service"`      //where the job comes from (e.g. importing user from excel, making report, etc)
+	Bucket      string                 `json:"bucket"`       //bucket of event
+	BucketKey   string                 `json:"bucket_key"`   //bucket key
+	AccessToken string                 `json:"access_token"` //Access Token of the user
+	Language    string                 `json:"language"`     //Language specification
+	Body        map[string]interface{} `json:"body"`         //Additional body
 }
