3a12a9df467181f1c364b062a3b6eee9e3719065
Kendrick Kesley <3642811@student.rmit.edu.au>
2018-10-31 16:13:38 +1100

    add access token in job

--- itcjob/Job.go
+++ itcjob/Job.go
@@ -2,9 +2,10 @@ package itcjob
 
 //Job holds the job
 type Job struct {
-	JobID     string `json:"job_id"`     //id of the job
-	Principal string `json:"principal"`  //arn of the requester
-	Service   string `json:"service"`    //where the job comes from (e.g. importing user from excel, making report, etc)
-	Bucket    string `json:"bucket"`     //bucket of event
-	BucketKey string `json:"bucket_key"` //bucket key
+	JobID       string `json:"job_id"`       //id of the job
+	Principal   string `json:"principal"`    //arn of the requester
+	Service     string `json:"service"`      //where the job comes from (e.g. importing user from excel, making report, etc)
+	Bucket      string `json:"bucket"`       //bucket of event
+	BucketKey   string `json:"bucket_key"`   //bucket key
+	AccessToken string `json:"access_token"` //Access Token of the user
 }
