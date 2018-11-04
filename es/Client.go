bb2b73153bdbea96b728751d114ce7344acb651f
kkesley <kendrick.kesley.dev@gmail.com>
2018-11-04 16:51:03 +1100

    add new with creds

--- es/Client.go
+++ es/Client.go
@@ -2,6 +2,7 @@ package es
 
 import (
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/aws/signer/v4"
 	"github.com/olivere/elastic"
@@ -14,7 +15,12 @@ func New(url string, region string) (*elastic.Client, error) {
 		Region: aws.String(region),
 	}
 	sess := session.Must(session.NewSession(&config))
-	signer := v4.NewSigner(sess.Config.Credentials)
+	return NewWithSession(url, region, sess)
+}
+
+//NewWithCreds new client with existing credentials
+func NewWithCreds(url string, region string, creds *credentials.Credentials) (*elastic.Client, error) {
+	signer := v4.NewSigner(creds)
 	awsClient, err := aws_signing_client.New(signer, nil, "es", region)
 	if err != nil {
 		return nil, err
@@ -29,15 +35,6 @@ func New(url string, region string) (*elastic.Client, error) {
 
 //NewWithSession generate new client with existing session
 func NewWithSession(url string, region string, sess *session.Session) (*elastic.Client, error) {
-	signer := v4.NewSigner(sess.Config.Credentials)
-	awsClient, err := aws_signing_client.New(signer, nil, "es", region)
-	if err != nil {
-		return nil, err
-	}
-	return elastic.NewClient(
-		elastic.SetURL(url),
-		elastic.SetScheme("https"),
-		elastic.SetHttpClient(awsClient),
-		elastic.SetSniff(false), // See note below
-	)
+	return NewWithCreds(url, region, sess.Config.Credentials)
+
 }
