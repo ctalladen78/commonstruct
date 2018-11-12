8501381ae0015163691ee0a37a41fb3a95c78161
Kendrick Kesley <3642811@student.rmit.edu.au>
2018-11-13 10:36:44 +1100

    add dynamodb helper

---
+++ dynamo/UnmarshalStreamImage.go
@@ -0,0 +1,31 @@
+package dynamo
+
+import (
+	"encoding/json"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+)
+
+// UnmarshalStreamImage converts events.DynamoDBAttributeValue to struct
+func UnmarshalStreamImage(attribute map[string]events.DynamoDBAttributeValue, out interface{}) error {
+
+	dbAttrMap := make(map[string]*dynamodb.AttributeValue)
+
+	for k, v := range attribute {
+
+		var dbAttr dynamodb.AttributeValue
+
+		bytes, marshalErr := v.MarshalJSON()
+		if marshalErr != nil {
+			return marshalErr
+		}
+
+		json.Unmarshal(bytes, &dbAttr)
+		dbAttrMap[k] = &dbAttr
+	}
+
+	return dynamodbattribute.UnmarshalMap(dbAttrMap, out)
+
+}
