380fb0b8787c143b944681c69178bae00f0f2e7b
Kendrick Kesley <3642811@student.rmit.edu.au>
2018-11-19 16:33:46 +1100

    user misc to edvmodel

--- es/edvmodel/EsUser.go
+++ es/edvmodel/EsUser.go
@@ -2,8 +2,6 @@ package edvmodel
 
 import (
 	"time"
-
-	"bitbucket.org/edvise/edv-platform-api/db/model/dynamodel"
 )
 
 //EsUser holds the user in elastic search
@@ -16,8 +14,8 @@ type EsUser struct {
 	Principal *string `json:"principal,omitempty"`
 	ClientARN *string `json:"client_arn,omitempty"`
 
-	Active    *bool                    `json:"active,omitempty"`
-	ParentExt *dynamodel.UserParentExt `json:"parent_ext,omitempty"`
+	Active    *bool          `json:"active,omitempty"`
+	ParentExt *UserParentExt `json:"parent_ext,omitempty"`
 
 	CreatedAt *time.Time `json:"created_at,omitempty"`
 	UpdatedAt *time.Time `json:"updated_at,omitempty"`
@@ -39,16 +37,43 @@ type EsParent struct {
 
 //EsStudent holds the student in elastic search
 type EsStudent struct {
-	FirstName          *string                `json:"first_name"`
-	LastName           *string                `json:"last_name"`
-	Phone              *string                `json:"phone"`
-	Email              *string                `json:"email"`
-	Gender             *string                `json:"gender"`
-	Religion           *string                `json:"religion"`
-	DateOfBirth        *string                `json:"date_of_birth"`
-	PlaceOfBirth       *string                `json:"place_of_birth"`
-	Relationship       *string                `json:"relationship"`
-	PasswordNeedChange *bool                  `json:"password_need_change"`
-	Medical            *dynamodel.UserMedical `json:"medical"`
-	Address            *dynamodel.UserAddress `json:"address"`
+	FirstName          *string      `json:"first_name"`
+	LastName           *string      `json:"last_name"`
+	Phone              *string      `json:"phone"`
+	Email              *string      `json:"email"`
+	Gender             *string      `json:"gender"`
+	Religion           *string      `json:"religion"`
+	DateOfBirth        *string      `json:"date_of_birth"`
+	PlaceOfBirth       *string      `json:"place_of_birth"`
+	Relationship       *string      `json:"relationship"`
+	PasswordNeedChange *bool        `json:"password_need_change"`
+	Medical            *UserMedical `json:"medical"`
+	Address            *UserAddress `json:"address"`
+}
+
+//UserMedical holds the medical condition of the student
+type UserMedical struct {
+	MedicalCondition *string `json:"medical_condition,omitempty"`
+}
+
+//UserAddress holds the primary address of the student
+type UserAddress struct {
+	Address  *string `json:"address,omitempty"`
+	State    *string `json:"state,omitempty"`
+	City     *string `json:"city,omitempty"`
+	PostCode *string `json:"post_code,omitempty"`
+	Country  *string `json:"country,omitempty"`
+}
+
+//UserParentExt holds additional parent info
+type UserParentExt struct {
+	FirstName    *string `json:"first_name"`
+	LastName     *string `json:"last_name"`
+	Email        *string `json:"email"`
+	Phone        *string `json:"phone"`
+	Gender       *string `json:"gender"`
+	Religion     *string `json:"religion"`
+	DateOfBirth  *string `json:"date_of_birth"`
+	PlaceOfBirth *string `json:"place_of_birth"`
+	Relationship *string `json:"relationship"`
 }
