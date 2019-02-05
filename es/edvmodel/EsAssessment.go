11361e16c21235f634ac0ec25ecbd68d468c3fcd
Kendrick Kesley <3642811@student.rmit.edu.au>
2019-02-05 11:53:45 +1100

    turn map into array of key value

--- es/edvmodel/EsAssessment.go
+++ es/edvmodel/EsAssessment.go
@@ -12,13 +12,13 @@ type EsAssessment struct {
 	Title          *string  `json:"title,omitempty"`
 	Description    *string  `json:"description,omitempty"`
 	Attachments    []string `json:"attachments,omitempty"`
-	Weight         *string `json:"weight,omitempty"` //out of 100
+	Weight         *string  `json:"weight,omitempty"` //out of 100
 
-	ManualPublished        *bool                 `json:"manual_published,omitempty"`
-	ReleaseDate            *time.Time            `json:"release_date,omitempty"`
-	DeadlineDate           *time.Time            `json:"deadline_date,omitempty"`
-	CustomDeadlineDate     map[string]*time.Time `json:"custom_deadline_date,omitempty"`
-	CustomDeadlineSchedule map[string]string     `json:"custom_deadline_schedule,omitempty"`
+	ManualPublished        *bool                        `json:"manual_published,omitempty"`
+	ReleaseDate            *time.Time                   `json:"release_date,omitempty"`
+	DeadlineDate           *time.Time                   `json:"deadline_date,omitempty"`
+	CustomDeadlineDate     []AssessmentDeadlineDate     `json:"custom_deadline_date,omitempty"`
+	CustomDeadlineSchedule []AssessmentDeadlineSchedule `json:"custom_deadline_schedule,omitempty"`
 
 	IsRemedial                 *bool   `json:"is_remedial,omitempty"`
 	RemedialOriginalAssessment *string `json:"remedial_original_assessment,omitempty"`
@@ -30,4 +30,16 @@ type EsAssessment struct {
 	CreatedAt *time.Time `json:"created_at,omitempty"`
 	UpdatedAt *time.Time `json:"updated_at,omitempty"`
 	UpdatedBy *string    `json:"updated_by,omitempty"`
+}
+
+//AssessmentDeadlineDate holds the array of key, value of assessment date
+type AssessmentDeadlineDate struct {
+	Key  *string    `json:"key,omitempty"`
+	Date *time.Time `json:"date,omitempty"`
+}
+
+//AssessmentDeadlineSchedule holds the array of key, value of assessment schedule
+type AssessmentDeadlineSchedule struct {
+	Key      *string `json:"key,omitempty"`
+	Schedule *string `json:"schedule,omitempty"`
 }
