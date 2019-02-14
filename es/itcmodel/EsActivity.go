55073b41cf1c77201a8c790372ace7292867500e
Kendrick Kesley <3642811@student.rmit.edu.au>
2019-02-15 10:20:15 +1100

    modify esactivity

--- es/itcmodel/EsActivity.go
+++ es/itcmodel/EsActivity.go
@@ -6,12 +6,22 @@ import (
 
 //EsActivity holds the user activity in the portal
 type EsActivity struct {
-	UserARN    *string    `json:"user_arn,omitempty"`
-	ClientARN  *string    `json:"client_arn,omitempty"`
-	Session    *string    `json:"session,omitempty"`
-	Action     *string    `json:"action,omitempty"`
-	TargetType *string    `json:"target_type,omitempty"`
-	TargetOld  *string    `json:"target_old,omitempty"`
-	TargetNew  *string    `json:"target_new,omitempty"`
-	CreatedAt  *time.Time `json:"created_at,omitempty"`
+	ClientARN   *string         `json:"client_arn,omitempty"`
+	Session     *string         `json:"session,omitempty"`
+	Action      *string         `json:"action,omitempty"`
+	TargetType  *string         `json:"target_type,omitempty"`  //CLASS_TO_STUDENT/STUDENT
+	TargetScope *string         `json:"target_scope,omitempty"` //EDVISE/PLATFORM
+	TargetOld   []ActivityValue `json:"target_old,omitempty"`   //FOR UPDATING/DELETING
+	TargetNew   []ActivityValue `json:"target_new,omitempty"`
+	Subjects    []ActivityValue `json:"subjects,omitempty"` //OPTIONAL FIELD. IF THE ACTION HAS A DIRECT SUBJECT (E.G. SCORING STUDENTS, SUBJECTS WILL BE STUDENT ARN, TARGET WILL BE THE SCORE)
+
+	Principal      *string    `json:"principal,omitempty"`
+	PrincipalScope *string    `json:"principal_scope,omitempty"`
+	CreatedAt      *time.Time `json:"created_at,omitempty"`
+}
+
+//ActivityValue represent a resource
+type ActivityValue struct {
+	Key   *string `json:"key"`
+	Value *string `json:"value"`
 }
