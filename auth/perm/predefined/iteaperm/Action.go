9c0b9e1907690f80b435ceb57fb95eb228450d6e
Kendrick Kesley <3642811@student.rmit.edu.au>
2019-02-15 12:54:26 +1100

    add activity log

--- auth/perm/predefined/iteaperm/Action.go
+++ auth/perm/predefined/iteaperm/Action.go
@@ -13,6 +13,7 @@ type PlatformAction struct {
 	PasswordPolicy PasswordPolicyAction `json:"password_policy"`
 	Billing        BillingAction        `json:"billing"`
 	Comment        CommentAction        `json:"comment"`
+	ActivityLog    ActivityLogAction    `json:"activity_log"`
 }
 
 //UserAction holds action of user
@@ -35,6 +36,11 @@ type RoleAction struct {
 type PasswordPolicyAction struct {
 	Read  string `json:"read"`
 	Write string `json:"write"`
+}
+
+//ActivityLogAction holds action of activity-log
+type ActivityLogAction struct {
+	Read string `json:"read"`
 }
 
 //BillingAction holds action for billings
@@ -93,6 +99,9 @@ var Action = ActionConstant{
 		Comment: CommentAction{
 			Read:  Permission.Platform.Comment + "::" + "read",
 			Write: Permission.Platform.Comment + "::" + "write",
+		},
+		ActivityLog: ActivityLogAction{
+			Read: Permission.Platform.ActivityLog + "::" + "read",
 		},
 	},
 }
--- auth/perm/predefined/iteaperm/Permission.go
+++ auth/perm/predefined/iteaperm/Permission.go
@@ -7,18 +7,20 @@ type PermissionConstant struct {
 
 //PlatformConstant hold the permission strings in platform
 type PlatformConstant struct {
-	User    string `json:"user"`
-	Role    string `json:"role"`
-	Billing string `json:"billing"`
-	Comment string `json:"comment"`
+	User        string `json:"user"`
+	Role        string `json:"role"`
+	Billing     string `json:"billing"`
+	Comment     string `json:"comment"`
+	ActivityLog string `json:"activity-log"`
 }
 
 //Permission predefined permission
 var Permission = PermissionConstant{
 	Platform: PlatformConstant{
-		User:    "itea::platform::user",
-		Role:    "itea::platform::role",
-		Billing: "itea::platform::billing",
-		Comment: "edvise::education::comment",
+		User:        "itea::platform::user",
+		Role:        "itea::platform::role",
+		Billing:     "itea::platform::billing",
+		Comment:     "itea::platform::comment",
+		ActivityLog: "itea::platform::activity-log",
 	},
 }
