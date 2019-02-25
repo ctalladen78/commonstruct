cc59800dfa262b41f50f00026c4c90b984675150
Kendrick Kesley <3642811@student.rmit.edu.au>
2019-02-26 10:26:40 +1100

    use pointer

--- notification/Payload.go
+++ notification/Payload.go
@@ -2,7 +2,7 @@ package notification
 
 //Payload of a message
 type Payload struct {
-	Title   string            `json:"title"`
-	Message string            `json:"message"`
+	Title   *string           `json:"title,omitempty"`
+	Message *string           `json:"message,omitempty"`
 	Data    map[string]string `json:"data"`
 }
