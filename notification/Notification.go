af25672c3967964e23eb6cbd89d9ff330c7e6f01
Kendrick Kesley <3642811@student.rmit.edu.au>
2019-02-25 15:29:54 +1100

    add external id in notification

--- notification/Notification.go
+++ notification/Notification.go
@@ -8,4 +8,5 @@ type Notification struct {
 	DeviceType  *string    `json:"device_type"`
 	Message     *string    `json:"message"`
 	SendTime    *time.Time `json:"send_time"`
+	ExternalID  *string    `json:"external_id"` //this id is used for scheduling. Must be unique accross the system
 }
