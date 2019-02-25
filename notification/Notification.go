ef52cee62a2201481a5a52d30e8591d74b12fee8
Kendrick Kesley <3642811@student.rmit.edu.au>
2019-02-26 10:21:18 +1100

    notification use payload

--- notification/Notification.go
+++ notification/Notification.go
@@ -6,7 +6,7 @@ import "time"
 type Notification struct {
 	TargetUsers []string   `json:"target_users"`
 	DeviceType  *string    `json:"device_type"`
-	Message     *string    `json:"message"`
+	Payload     Payload    `json:"payload"`
 	SendTime    *time.Time `json:"send_time"`
 	ExternalID  *string    `json:"external_id"` //this id is used for scheduling. Must be unique accross the system
 	Disabled    *bool      `json:"disabled"`    //if not enabled, ignore this notification
