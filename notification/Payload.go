63c97fc841cb95dbde91c37536654a76aec5134f
Kendrick Kesley <3642811@student.rmit.edu.au>
2019-02-26 11:19:59 +1100

    add convert method

--- notification/Payload.go
+++ notification/Payload.go
@@ -7,3 +7,30 @@ type Payload struct {
 	Data       map[string]string `json:"data"`
 	GroupingID *string           `json:"grouping_id"`
 }
+
+//ConvertToNotification converts payload to a notification object
+func (p Payload) ConvertToNotification() Platform {
+	return Platform{
+		Default: p.Title,
+		GCM: &GCM{
+			Notification: &GCMNotification{
+				Title: p.Title,
+				Text:  p.Message,
+			},
+			Config: &AndroidConfig{
+				CollapseKey: p.GroupingID,
+			},
+			Data: p.Data,
+		},
+		APNS: &APNS{
+			Notification: &APNSNotification{
+				Alert: &APNSAlert{
+					Title: p.Title,
+					Body:  p.Message,
+				},
+				ThreadID: p.GroupingID,
+			},
+			Data: p.Data,
+		},
+	}
+}
