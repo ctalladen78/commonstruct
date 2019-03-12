e8e960fe9f44ba5c11cbdec8d08fcf42d857a9c6
Kendrick Kesley <3642811@student.rmit.edu.au>
2019-03-12 11:10:50 +1100

    save score too

--- es/edvmodel/EsReport.go
+++ es/edvmodel/EsReport.go
@@ -23,19 +23,20 @@ type EsReport struct {
 
 	ReleaseDate *time.Time `json:"release_date,omitempty"`
 
-	Principal             *string                        `json:"principal,omitempty"`
-	CreatedAt             *time.Time                     `json:"created_at,omitempty"`
-	UpdatedAt             *time.Time                     `json:"updated_at,omitempty"`
-	UpdatedBy             *string                        `json:"updated_by,omitempty"`
-	AssessmentTypeWeights []EsReportAssessmentTypeWeight `json:"assessment_type_weights,omitempty"`
-	CommentSections       []EsReportCommentSection       `json:"comment_sections,omitempty"`
+	Principal            *string                        `json:"principal,omitempty"`
+	CreatedAt            *time.Time                     `json:"created_at,omitempty"`
+	UpdatedAt            *time.Time                     `json:"updated_at,omitempty"`
+	UpdatedBy            *string                        `json:"updated_by,omitempty"`
+	AssessmentTypeScores []EsReportAssessmentTypeScores `json:"assessment_type_scores,omitempty"`
+	CommentSections      []EsReportCommentSection       `json:"comment_sections,omitempty"`
 }
 
-//EsReportAssessmentTypeWeight holds the assessment type weight
-//e.g. "EXAM" => 20%
-type EsReportAssessmentTypeWeight struct {
-	AssessmentType *string  `json:"assessment_type"`
-	Weight         *float64 `json:"weight"`
+//EsReportAssessmentTypeScores hold the assessment type accumulated scores (unweighted)
+type EsReportAssessmentTypeScores struct {
+	AssessmentType  *string  `json:"assessment_type"`
+	Weight          *float64 `json:"weight"`
+	UnweightedScore *float64 `json:"unweighted_score"`
+	WeightedScore   *float64 `json:"weighted_score"`
 }
 
 //EsReportCommentSection holds the comment for a section in the report
